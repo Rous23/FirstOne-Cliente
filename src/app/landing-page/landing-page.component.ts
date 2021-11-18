43c2f98fc3dd51495477938d7fb3414f5f02eace
Luisjimenez2812 <luisjimenez2812@gmail.com>
2021-11-17 22:52:21 -0600

    Landing page terminada

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBookOpen, faDolly } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPageComponent implements OnInit {
 
   constructor() { }
+
+  faBookOpen = faBookOpen
+  faDolly = faDolly
 
   ngOnInit(): void {
   }
